fb61fd0a0e1f8967dd7598d7c5f9f42d2288389a
Mattia Natali <matitalatina@gmail.com>
2022-09-22 16:44:17 +0200

    First commit

---
+++ site-config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  // App Info
+  appstore_link         : "https://apps.apple.com/us/app/metext-text-scanner-ocr/id1510120975",              // Enter App Store URL.
+  playstore_link        : "https://play.google.com/store/apps/details?id=it.mattianatali.metext",    // Enter Google Play Store URL.
+  google_analytics_ID   : "disabled",                                       // Enter Google Analytics ID or ""
+  presskit_download_link: null,                                                   // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file). 
+  video_or_screenshot   : "screenshot",                                           // "screenshot" or "video"
+  app_url               : "https://metext.mattianatali.com",                       // Domain of your website without path_prefix.
+  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "Metext",
+  app_price             : "Free",
+  app_description       : "Extract text from pictures!",
+  app_keywords          : ["ocr", "text recognition", "machine learning"],
+
+  // Personal Info
+  your_name              : "Mattia Natali",
+  your_link              : "https://blog.mattianatali.dev",
+  your_city              : "Milano 🇮🇹",
+  email_address          : "me@mattianatali.it",
+  facebook_username      : null,
+  linkedin_username      : "mattian",
+  instagram_username     : null,
+  twitter_username       : null,
+  github_username        : "matitalatina",
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "Extract the text from images",
+      description:
+        "We're using the power of Google AI to extract text. The best-in-class algorithm is under your fingertips!",
+      fontawesome_icon_name: "font",
+    },
+    {
+      title: "Blazingly fast",
+      description:
+        "Just three taps and you're done.",
+      fontawesome_icon_name: "rocket",
+    },
+    {
+      title: "Share with anyone",
+      description:
+        "Sharing the extracted text is very easy: just send it to whoever you want.",
+      fontawesome_icon_name: "share-nodes",
+    },
+  ],
+  header_background             : "rgba(0, 0, 0, 0.1)",
+  topbar_title_color            : "#fff",
+  cover_overlay_color_rgba_1      : "rgba(0, 10, 2, 0.8) 80%",
+  cover_overlay_color_rgba_2      : "rgba(0, 10, 2, 1)",
+  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "#000",
+  primary_text_color            : "#fff",
+  content_width                 : "1170px",
+  font                          : `"Helvetica Neue", sans-serif`,
+  link_color                    : "#B8FE6C",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_description_color         : "#ffffff",
+  feature_title_color           : "#fff",
+  feature_text_color            : "#666666",
+  feature_icons_foreground_color: "#B8FE6C",
+  feature_icons_background_color: "#222",
+  social_icons_foreground_color : "#000",
+  social_icons_background_color : "#444",
+  footer_text_color             : "#666666",
+}
---
+++ src/components/seo.js
@@ -0,0 +1,119 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+import urljoin from "url-join";
+
+function SEO({ description, lang, meta, keywords, title }) {
+  const { seoImage, site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            siteUrl
+          }
+        }
+        seoImage: file(relativePath: { eq: "landing-cover.jpg" }) {
+          childImageSharp {
+            fixed(width: 1600) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description;
+  const seoImageUrl = urljoin(site.siteMetadata.siteUrl, seoImage.childImageSharp.fixed.src);
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={site.siteMetadata.title}
+      meta={[
+        {
+          name: 'google-site-verification',
+          content: 'ePMriV_-H2lldLkXE2ys9SWHsCYVxNO0z4DdGOn0il0',
+        },
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:image`,
+          content: seoImageUrl,
+        },
+        {
+          property: `twitter:image`,
+          content: seoImageUrl,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ]
+        .concat(
+          keywords.length > 0
+            ? {
+                name: `keywords`,
+                content: keywords.join(`, `),
+              }
+            : []
+        )
+        .concat(meta)}
+    />
+  )
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  keywords: [],
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.array,
+  keywords: PropTypes.arrayOf(PropTypes.string),
+  title: PropTypes.string.isRequired,
+}
+
+export default SEO
